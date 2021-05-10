8cfbdfb0f6b1277e4e923e2ab7138599098ac237
oleksandr-burlakov <83636875+oleksandr-burlakov@users.noreply.github.com>
2021-05-10 15:50:02 +0300

    Added MathResult class and make dependence between match and matchstate class

--- VGame/VGame.CL/Match.cs
+++ VGame/VGame.CL/Match.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace VGame.CL
 {
     public class Match
     {
-        
+        public int Id { get; set; }
+        public int FirstTeamId { get; set; }
+        public int SecondTeamId { get; set; }
+        public Team FirstTeam { get; set; }
+        public Team SecondTeam { get; set; }
+        public DateTime MatchDate { get; set; }
+        public MatchResult MatchResult { get; set; }
     }
 }
