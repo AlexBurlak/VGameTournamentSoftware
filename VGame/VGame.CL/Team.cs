d79def83d9f48cc90d7860f5e2a92e6f374bef46
oleksandr-burlakov <83636875+oleksandr-burlakov@users.noreply.github.com>
2021-05-10 15:43:58 +0300

    Added team and empty match model

--- VGame/VGame.CL/Team.cs
+++ VGame/VGame.CL/Team.cs
@@ -1,7 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace VGame.CL
 {
     public class Team
     {
-        
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationStamp { get; set; }
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Match> Matches { get; set; }
     }
 }
